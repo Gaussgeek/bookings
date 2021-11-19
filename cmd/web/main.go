ff6ea1c82eb8485cea8422dc1fd9e649516c535e
Andrew Lukyamuzi <rokyamuzi@gmail.com>
2021-11-19 20:41:07 +0300

    updated forms and added reservation summary template,forms,errors, with redirection and popup messages

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/gob"
 	"fmt"
 	"github.com/Gaussgeek/bookings/internal/config"
 	"github.com/Gaussgeek/bookings/internal/handlers"
+	"github.com/Gaussgeek/bookings/internal/models"
 	"github.com/Gaussgeek/bookings/internal/render"
 	"github.com/alexedwards/scs/v2"
 	"log"
@@ -18,6 +20,9 @@ var session *scs.SessionManager
 
 // main is the main function
 func main() {
+	// what am I going to put in the session
+	gob.Register(models.Reservation{})
+
 	// change this to true when in production
 	app.InProduction = false
 
