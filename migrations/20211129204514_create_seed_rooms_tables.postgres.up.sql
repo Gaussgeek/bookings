563b9545d02a471133dac93c6683c5396be2a576
Andrew Lukyamuzi <rokyamuzi@gmail.com>
2021-11-30 00:10:13 +0300

    app after section 13

---
+++ migrations/20211129204514_create_seed_rooms_tables.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (room_name,created_at,updated_at) VALUES
+	 ('Presidential suite','2021-11-27 00:00:00.000','2021-11-27 00:00:00.000'),
+	 ('Platinum room','2021-11-28 00:00:00.000','2021-11-28 00:00:00.000');
